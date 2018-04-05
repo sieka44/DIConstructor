4f11aa66b713c3323f10c5bf3a3c60080d0e906a
sieka44 <sieka444@interia.pl>
2018-04-05 11:16:34 +0200

    README update.

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ Wstrzykiwanie zależności do konstruktora - String
 ## How To Start
 
 1. Download project.
-2. Run app with "Spring Main"
+2. Run app with "Spring Main" to use default file: "movies.txt"
+
+Or
+
+1. Run jar file with one parameter: "file name" ex
+    
+```
+java -jar DI.jar movies.txt
+```
--- src/main/java/uj/jwzp/w4/launcher/SpringMain.java
+++ src/main/java/uj/jwzp/w4/launcher/SpringMain.java
@@ -1,4 +1,4 @@
-package uj.jwzp.w4.launchers;
+package uj.jwzp.w4.launcher;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
@@ -11,11 +11,20 @@ import uj.jwzp.w4.model.Movie;
 @Slf4j
 public class SpringMain {
 
-    private static final String FILE_NAME = "movies.txt";
+    private static final String DEFAULT_FILE_NAME = "movies.txt";
 
     public static void main(String[] args) {
+        /**/
+        String fileName = "";
+        if(args.length > 0){
+            fileName = args[0];
+        }else {
+         fileName=DEFAULT_FILE_NAME;
+        }
+        /**/
         AnnotationConfigApplicationContext configApplicationContext = new AnnotationConfigApplicationContext();
-        configApplicationContext.registerBean(CSVMovieFinder.class, () -> new CSVMovieFinder(FILE_NAME));
+        String finalFileName = fileName;
+        configApplicationContext.registerBean(CSVMovieFinder.class, () -> new CSVMovieFinder(finalFileName));
         configApplicationContext.scan("uj.jwzp.w4.logic.include");
         configApplicationContext.refresh();
         ApplicationContext ctx = configApplicationContext;
